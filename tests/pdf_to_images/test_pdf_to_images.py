b975dce570752080d89dd916634aef6266bd1bba
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-30 15:31:46 +0530

    test for pdf to images

---
+++ tests/pdf_to_images/test_pdf_to_images.py
@@ -0,0 +1,21 @@
+import shutil
+from pathlib import Path 
+
+from ocr_handwriting_aligner.utility import pdf_to_images
+
+
+def test_pdf_to_images():
+    DATA_DIR = Path(__file__).parent / "data"
+    pdf_file_path = DATA_DIR / "potrait_cursive_handwriting.pdf"
+
+    result = pdf_to_images(pdf_file_path, DATA_DIR)
+    assert "images_path" in result
+    assert len(result["images_path"]) == 3
+    for image_path in result["images_path"]:
+        assert image_path.exists()
+    
+    """ Clean up"""
+    shutil.rmtree(DATA_DIR / pdf_file_path.stem, ignore_errors=True)
+
+
+test_pdf_to_images()
