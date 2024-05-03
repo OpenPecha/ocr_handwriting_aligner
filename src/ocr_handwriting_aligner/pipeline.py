512ab6c8e5fca92f182e31c71b59133d22353ab0
Tenzin <tenzintsunduebhattu@gmail.com>
2024-05-03 12:22:11 +0530

    map good line image to transcript

--- src/ocr_handwriting_aligner/pipeline.py
+++ src/ocr_handwriting_aligner/pipeline.py
@@ -1,3 +1,4 @@
+import csv 
 from pathlib import Path 
 from typing import List
 
@@ -7,7 +8,7 @@ from ocr_handwriting_aligner.utils import get_coordinates_from_xml, standardize_
 from ocr_handwriting_aligner.image_utils import crop_image
 from ocr_handwriting_aligner.config import PORTRAIT_LINE_IMAGES_COORDINATES_XML_PATH, PORTRAIT_LINE_IMAGES_LABEL_COORDINATES_XML_PATH
 from ocr_handwriting_aligner.quality_classifier import is_image_quality_acceptable
-
+from ocr_handwriting_aligner.parse_transcript import get_line_image_transcript
 
 def crop_image_pipeline(image_path: Path, output_dir:Path, xml_path:Path):
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -18,7 +19,7 @@ def crop_image_pipeline(image_path: Path, output_dir:Path, xml_path:Path):
             cropped_image.save(output_dir / f"{image_path.stem}_{idx+1}.jpg")
 
 
-def pipeline(images_path: List[Path]):
+def pipeline(images_path: List[Path], transcript_file_path:Path, output_csv_path:Path=Path("line_image_mapping.csv")):
     """ This function is the main pipeline function that will be called to crop the line images from the given images"""
     line_image_dir = Path("cropped_line_images")
     line_image_label_dir = Path("cropped_line_images_label") 
@@ -44,7 +45,7 @@ def pipeline(images_path: List[Path]):
     images_path = list(line_image_dir.rglob("*.jpg"))
     images_path = sort_paths_and_get_paths(images_path)
 
-    acceptable_images_path = []
+    acceptable_images = []
     for image_path in tqdm(images_path, desc="Getting acceptable line images"):
         """ get line image label path """
         label_image_path = line_image_label_dir / image_path.parent.stem / image_path.name
@@ -53,13 +54,26 @@ def pipeline(images_path: List[Path]):
             print(f"Label image not found for {str(image_path)}")
             continue
 
+        """ check if the image quality is acceptable"""
+        """ if the image quality is acceptable, then get the transcript for the line image"""
         if is_image_quality_acceptable(label_image_path):
-            acceptable_images_path.append(image_path)
-    
-    return acceptable_images_path
+            image_name = image_path.parent.name
+            image_number = int(image_name.split("_")[-1])
+            line_number = int(image_path.stem.split("_")[-1])
+            image_transcript = get_line_image_transcript(transcript_file_path, image_number, line_number)
+            if image_transcript["text"] :
+                acceptable_images.append({"images_path": str(image_path), "transcripts": image_transcript["text"]})
+
+    """ write the mapping to csv"""
+    with open(output_csv_path, 'w') as csvfile: 
+        writer = csv.DictWriter(csvfile, fieldnames = ["images_path", "transcripts"]) 
+        writer.writeheader() 
+        writer.writerows(acceptable_images) 
+    return acceptable_images
 
 if __name__ == "__main__":
     images_path = list(Path("images_output").rglob("*.jpg"))
-    result = pipeline(images_path)
+    transcript_file_path = Path("P000010_v001.csv")
+    acceptable_images = pipeline(images_path, transcript_file_path)
     print(f"Number of line images: {len(images_path)*7}")
-    print(f"Number of acceptable line images: {len(result)}")
+    print(f"Number of acceptable line images: {len(acceptable_images)}")
