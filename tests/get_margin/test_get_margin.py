b4f298df87cbd258fa60140b0b3654e86d6118b9
Tenzin <tenzintsunduebhattu@gmail.com>
2024-05-07 10:59:40 +0530

    update more precise margin coordinate in test

--- tests/get_margin/test_get_margin.py
+++ tests/get_margin/test_get_margin.py
@@ -7,10 +7,12 @@ def test_get_image_margin():
     DATA_DIR = Path(__file__).parent / "data"
     image_path = str(DATA_DIR / "potrait_image.jpg")
     margin_coordinate = get_image_margin(image_path)
-    assert margin_coordinate == {'Top-Left': (74, 57), 'Bottom-Right': (1578, 2256)}
+    assert margin_coordinate == {'Top-Left': (74, 57), 'Bottom-Right': (1578, 2257)}
 
     DATA_DIR = Path(__file__).parent / "data"
     image_path = str(DATA_DIR / "landscape_image.jpg")
     margin_coordinate = get_image_margin(image_path)
-    assert margin_coordinate == {'Top-Left': (72, 71), 'Bottom-Right': (1580, 2273)}
+    assert margin_coordinate == {'Top-Left': (72, 70), 'Bottom-Right': (1580, 2273)}
 
+
+test_get_image_margin()
