08b0e3f85f2da700d2a3ce4238dccd78f0619973
Tenzin <tenzintsunduebhattu@gmail.com>
2024-05-08 11:25:29 +0530

    modify file name for test

--- tests/pdf_to_images/test_pdf_to_images.py
+++ tests/pdf_to_images/test_pdf_to_images.py
@@ -6,23 +6,12 @@ from ocr_handwriting_aligner.image_utils import pdf_to_images
 
 def test_pdf_to_images():
     DATA_DIR = Path(__file__).parent / "data"
-    pdf_file_path = DATA_DIR / "potrait_cursive_handwriting.pdf"
+    pdf_file_path = DATA_DIR / "P000010_v001_00001 - 00003.pdf"
 
     result = pdf_to_images(pdf_file_path, DATA_DIR)
-    assert "images_path" in result
     assert len(result) == 3
     for image_path in result:
         assert image_path.exists()
     
     """ Clean up"""
     shutil.rmtree(DATA_DIR / pdf_file_path.stem, ignore_errors=True)
-
-    pdf_file_path = DATA_DIR / "does_not_exist.pdf"
-    result = pdf_to_images(pdf_file_path, DATA_DIR)
-
-    assert "error" in result
-    assert result["error"] == f"PDF file {str(pdf_file_path)} does not exist"
-
-    """ clean up """
-    shutil.rmtree(DATA_DIR / pdf_file_path.stem, ignore_errors=True)
-
